37d45c6e369a7d415d1fad7bb03b8f6137a9a503
Phil <phillip.haberern5@gmail.com>
2017-11-15 21:41:56 -0500

    enabled handlebars templating engine and started consuming json test data

--- public/js/dynamic.js
+++ public/js/dynamic.js
@@ -1,26 +1,56 @@
 $('document').ready(function(){
-		
-	//on click, redirect 
+
+	//on click, start exam
 	$("#start-button").on("click",function(){
+		console.log('start button fired');
 		var selection = $("#sat-section-selection").val();
-		
-		switch(selection){
-			case "mathCalcTemplate":
-				window.location.replace("./assets/templates/mathCalcTemplate.html");
-				break;
-			case "mathNoCalcTemplate":
-				window.location.replace("./assets/templates/mathNoCalcTemplate.html");
-				break;
-			case "readingTemplate":
-				window.location.replace("./assets/templates/readingTemplate.html");
-				break;
-			case "writingTemplate":
-				window.location.replace("./assets/templates/writingTemplate.html");
-				break;
-			default: alert("you screwed up");
-		
-			};			
-		
+		loadQuiz(selection);
 		});
-	
+
+	//***************************** function delcarations *************************
+
+    //functions for the timer______________________________________________________
+    function countdown() {
+        timerId = setInterval(function() {
+            counter--;
+            if (counter < 0) {
+                runReport();
+                clearInterval(timerId);
+                alert("Time is up!");
+            }
+            $("#timer").text(convertSeconds(counter));
+        }, 1000);
+    };
+
+    function convertSeconds(inputSeconds) {
+        var minutes = Math.floor(inputSeconds / 60);
+        var seconds = inputSeconds - (minutes * 60);
+        //return calculation
+        return (prettifyRemainingTime(minutes, "0", 2) + ":" + prettifyRemainingTime(seconds, "0", 2));
+    };
+
+    function prettifyRemainingTime(string, pad, length) {
+        return (new Array(length + 1).join(pad) + string).slice(-length);
+    };
+
+    //functions for quiz load_______________________________________________________
+    function loadQuiz(quizName){
+    	console.log('loadQuiz fired');
+    	$.getJSON(('./data/'+ quizName + '.json'), function(data){
+    		console.log(data);
+    		$('#intro').append('<h1>'+data.name+'</h1>');
+    	});
+    };
+
+
+
+
+
+
+
+
+
+
+
+
 });
